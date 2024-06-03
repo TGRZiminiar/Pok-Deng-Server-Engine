ee9ef86a338c62a4019630b5f189368846450af8
tgrziminiar <mixzazazaza1212@gmail.com>
2024-06-04 00:00:37 +0700

    feat:add deck, testing deck

---
+++ deck/deck.go
@@ -0,0 +1,129 @@
+package deck
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+)
+
+type Suit int
+
+func (s Suit) String() string {
+	switch s {
+	case Spades:
+		return "SPADES"
+	case Harts:
+		return "HARTS"
+	case Diamonds:
+		return "DIAMONDS"
+	case Clubs:
+		return "CLUBS"
+	default:
+		return "invalid card suit"
+		// panic()
+	}
+}
+
+const (
+	Spades   Suit = iota // 0
+	Harts                // 1
+	Diamonds             // 2
+	Clubs                // 3
+)
+
+type Card struct {
+	Suit  Suit
+	Value int
+}
+
+func (c Card) String() string {
+	value := strconv.Itoa(c.Value)
+	if c.Value == 1 {
+		value = "ACE"
+	}
+
+	return fmt.Sprintf("%s of %s %s", value, c.Suit, suitToUnicode(c.Suit))
+}
+func (c Card) SpecialCardValue(val int) string {
+	switch val {
+	case 1:
+		return "ACE"
+	case 11:
+		return "J"
+	case 12:
+		return "Q"
+	case 13:
+		return "K"
+	}
+	return strconv.Itoa(val)
+}
+
+func NewCard(s Suit, v int) Card {
+	if v > 13 {
+		panic("the value of the card cannot be higher then 13")
+	}
+
+	return Card{
+		Suit:  s,
+		Value: v,
+	}
+}
+
+type Deck [52]Card
+
+func NewDeck() Deck {
+	var (
+		nSuits = 4
+		nCards = 13
+		d      = [52]Card{}
+	)
+
+	x := 0
+	for i := 0; i < nSuits; i++ {
+		for j := 0; j < nCards; j++ {
+			d[x] = NewCard(Suit(i), j+1)
+			x++
+		}
+	}
+
+	return shuffle(d)
+}
+
+func shuffle(d Deck) Deck {
+	for i := 0; i < len(d); i++ {
+		r := rand.Intn(i + 1)
+
+		if r != i {
+			d[i], d[r] = d[r], d[i]
+		}
+	}
+
+	return d
+}
+
+func suitToUnicode(s Suit) string {
+	switch s {
+	case Spades:
+		return handleColor(34, "♠")
+	case Harts:
+		return handleColor(31, "❤️")
+	case Diamonds:
+		return handleColor(31, "♦")
+	case Clubs:
+		return handleColor(34, "♣")
+	default:
+		panic("invalid card suit")
+	}
+}
+
+// 30: Black
+// 31: Red
+// 32: Green
+// 33: Yellow
+// 34: Blue
+// 35: Magenta
+// 36: Cyan
+// 37: White
+func handleColor(ansicode int, text string) string {
+	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", ansicode, text)
+}
---
+++ deck/deck_test.go
@@ -0,0 +1,35 @@
+package deck
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	deck := NewDeck()
+	for _, v := range deck {
+		fmt.Println(suitToUnicode(v.Suit), v.SpecialCardValue(v.Value))
+	}
+	colored := fmt.Sprintf("\x1b[%dm%s\x1b[0m", 34, "Hello")
+	fmt.Println(colored)
+}
+
+func TestSuitString(t *testing.T) {
+	tests := []struct {
+		suit Suit
+		want string
+	}{
+		{Spades, "SPADES"},
+		{Harts, "HARTS"},
+		{Diamonds, "DIAMONDS"},
+		{Clubs, "CLUBS"},
+		{4, "invalid card suit"},
+	}
+
+	for _, tt := range tests {
+		got := tt.suit.String()
+		if got != tt.want {
+			t.Errorf("String() = %s; want %s", got, tt.want)
+		}
+	}
+}
